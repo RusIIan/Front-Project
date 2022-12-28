7c467beed3a4d5eb22dd228ba75f986053dbdeb3
Ruslan <ruslanri@code.edu.az>
2022-12-28 22:18:22 +0400

    lahiye

---
+++ assets/Js/main.js
@@ -0,0 +1,10 @@
+const cart = document.querySelector('.card');
+const price = document.querySelector('.price')
+
+
+
+cart.addEventListener('mouseenter',()=>  {
+    price.classList.add('ca')
+})
+cart.addEventListener('mouseleave',()=>{
+})
