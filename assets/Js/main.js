96d5c8c76761a6ba9aaa5e7ff05bbb345d10d5d5
Ruslan <ruslanri@code.edu.az>
2022-12-31 00:49:50 +0400

    lahiye

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,10 +1,37 @@
-const cart = document.querySelector('.card');
-const price = document.querySelector('.price')
+/* Устанавливаем индекс слайда по умолчанию */
+let slideIndex = 1;
+showSlides(slideIndex);
 
+/* Увеличиваем индекс на 1 — показываем следующий слайд*/
+function nextSlide() {
+  showSlides((slideIndex += 1));
+}
 
+/* Уменьшает индекс на 1 — показываем предыдущий слайд*/
+function previousSlide() {
+  showSlides((slideIndex -= 1));
+}
 
-cart.addEventListener('mouseenter',()=>  {
-    price.classList.add('ca')
-})
-cart.addEventListener('mouseleave',()=>{
-})
+/* Устанавливаем текущий слайд */
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+/* Функция перелистывания */
+function showSlides(n) {
+  let i;
+  let slides = document.querySelectorAll(".item");
+
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+
+  /* Проходим по каждому слайду в цикле for */
+  for (let slide of slides) {
+    slide.style.display = "none";
+  }
+  slides[slideIndex - 1].style.display = "block";
+}
